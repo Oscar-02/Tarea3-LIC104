999c0cf5e2d53bd8ee56c33aa22e4542a8cdb2ee
Oscar-02 <oscacanas29@gmail.com>
2022-11-02 21:47:59 -0600

    v0.2

---
+++ assets/contentLoader.js
@@ -0,0 +1,49 @@
+var urlGetCurrency = "https://api.exchangerate.host/symbols"
+var urlConvert = "https://api.exchangerate.host/convert?from=USD&to=EUR";
+var available = new Array();
+//[codigo, descripcion]
+
+fetch(urlGetCurrency)
+.then (response => {
+  return response.json();
+})
+.then (json => {
+  Object.keys(json["symbols"]).forEach(function(key){
+    available.push([json["symbols"][key]["code"], json["symbols"][key]["description"]]);
+  })
+  available.forEach(element => {
+    var country =  "flag: " + countryNames.of(element[0].substring(0, 2));
+    finalArray.forEach(subelement => {
+      if (subelement.includes(country)){
+        var emojiCode = subelement.substring(0, 11);
+        flagFormat.push([String.fromCodePoint(`0x${emojiCode.substring(0,5)}`)
+        + String.fromCodePoint(`0x${emojiCode.substring(6,11)}`), element[0], element[1]])
+      }
+    })
+})
+})
+
+//FUNCION CONVERTIR
+
+var urlEmojiList = "assets/emoji-sequences.txt"
+var emojiText = new String();
+var flagFormat = new Array();
+
+var emojiFile = new XMLHttpRequest();
+emojiFile.open("GET", urlEmojiList, false);
+emojiFile.onreadystatechange = () => {
+  if (emojiFile.readyState === 4){
+    if (emojiFile.status === 200 || emojiFile.status == 0){
+      emojiText = emojiFile.responseText;
+    }
+  }
+}
+emojiFile.send(null);
+
+var lineArray = emojiText.split('\n');
+var finalArray = new Array();
+for (i = 558; i < 816; i++){
+  finalArray.push(lineArray[i])
+}
+var countryNames = new Intl.DisplayNames(['en'], {type: 'region'});
+
--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-var urlGetCurrency = "https://api.exchangerate.host/symbols"
-var urlConvert = "https://api.exchangerate.host/convert?from=USD&to=EUR";
-var available = new Array();
-//[codigo, descripcion]
+var valueContain = document.getElementsByClassName('value');
 
-fetch(urlGetCurrency)
-.then (response => {
+var flag1 = document.getElementById('flag1');
+var flag2 = document.getElementById('flag2');
+var currency1 = document.getElementById('currency1');
+var currency1 = document.getElementById('currency2');
+var currencyName1 = document.getElementById('currencyName1');
+var currencyName2 = document.getElementById('currencyName2');
 
-  return response.json();
-})
-.then (json => {
-  Object.keys(json["symbols"]).forEach(function(key){
-    available.push([json["symbols"][key]["code"], json["symbols"][key]["description"]])
-  })
-})
+unfade(flag1);
+fade(flag1);
+
+function fade(element) {
+    var op = 1;
+    var timer = setInterval( () => {
+        if (op <= 0.1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ')';
+        op -= op * 0.1;
+    }, 50);
+}
+
+function unfade(element) {
+    var op = 0.1;
+    var timer = setInterval( () => {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ')';
+        op += op * 0.1;
+    }, 10);
+}
